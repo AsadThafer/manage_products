4ce845582a8a2f4c24c3d36eb52f8e561797360c
Asad Asad <asad.gamerz@gmail.com>
2022-08-24 08:38:29 +0300

    update method still needs some optimization

--- asadscript.js
+++ asadscript.js
@@ -1,4 +1,4 @@
-let prods = [];
+var order=12;
 $(document).ready(function () {
     
 $('#productform').validate({ // initialize the plugin
@@ -46,17 +46,34 @@ $('#productform').validate({ // initialize the plugin
         data.Description = Descriptionval;
         data.BadgeText = badgetextval;
         data.Color = colorrval;
+        data.id = Math.random() + productnameval;
         document.getElementById('productform').reset();
-        $("#outputready").append( 
-        `<div class="col-4"><input type="hidden" value='${JSON.stringify(data)}' > 
-          <img src="${data.imageURL}" width=100% height= 60% >
-          <p> ${data.ProductName}  ${data.Price}$ </br>
-             ${data.Description} </br>
-             ${data.BadgeText} </br>
-             ${data.Color}  </p>
-             <button type="button" class="btn btn-primary" onclick="edit(event)" >Update Product</button>
-             <button type="button" class="btn btn-danger" onclick="this.parentElement.style.display = 'none';">Delete Product</button>
+        $("#outputready").append(`
+           <div id="${data.id}" class="col-3 offset-1 order-12 productcard"><input type="hidden" value='${JSON.stringify(data)}'> 
+           <div class="row">
              
+           </div>
+           <div class="imgdiv">
+             <img src="${data.imageURL}" width="100%" height="60%">
+             <div class="badgeproduct">${data.BadgeText}</div>  
+           </div>
+ 
+           <div class="container-fluid productinformation float-sm-right">
+             <div class="row">
+               <div class="col text-left font-weight-bold text-uppercase">
+               ${data.ProductName}
+               </div>
+               <div class="col text-right">$${data.Price}</div>  
+             </div>
+             <div class="text-center text-secondary">${data.Description}</div> 
+             color:<div class="row-1 mb-4" style="background-color:${data.Color}; border: 1px;width: 20px;height: 20px;"></div> 
+             <div class="row   flex-nowrap">
+             <div class="col"><button type="button" class="btn btn-primary btn-block" onclick="edit(event)">Update</button></div>
+             <div class="col"><button type="button" name="deletebutton" class="btn btn-danger btn-block" onclick="checkbeforedelete()">Delete</button></div>
+ 
+             </div>
+         
+ 
            </div>
        `
         );
@@ -72,28 +89,36 @@ $('#productform').validate({ // initialize the plugin
 function edit(e){
     e = e || window.event;
     var target = e.target || e.srcElement;
-    json = target.parentNode.firstChild.value;
+    uppbttloc = target;
+    json = target.parentNode.parentNode.parentNode.parentNode.firstChild.value;
     obj = JSON.parse(json);
     addp = document.getElementById('AddP');
     addp.style.display = 'none';
     Savep = document.getElementById('SaveP');  
     Savep.style.display = 'block';
     var form = document.forms[0];
-    form.querySelector('input[name="imgurl"]').value =obj.imageURL;
-    form.querySelector('input[name="productname"]').value =obj.ProductName;
+    iu = form.querySelector('input[name="imgurl"]').value =obj.imageURL;
+    pn = form.querySelector('input[name="productname"]').value =obj.ProductName;
     form.querySelector('input[name="Price"]').value =obj.Price;
     form.querySelector('input[name="Description"]').value =obj.Description;
     form.querySelector('input[name="badgetext"]').value =obj.BadgeText;
     form.querySelector('input[name="colorr"]').value =obj.Color;
 
-    olddiv = target.parentNode;
-    Savep.onclick = () => {
-        addp.style.display = 'block';
-        Savep.style.display = 'none';
-        olddiv.style.display = 'none';
-        submitHandler();
+    Savep.onclick = (e) => {
+        e.preventDefault();
+        if ($("#productform").valid()) {
+            
+            addp.style.display = 'block';
+            Savep.style.display = 'none';
+
+            // updatebuttll=document.getElementById('Updatebutton');
+            // founditt= $(updatebuttll).closest('.productcard')[0];
+            // $(founditt).remove();
+        }
+        else{
+            alert('check product info');
+        }
     
-        document.getElementById('productform').reset();
         
         
       }
@@ -103,3 +128,14 @@ function edit(e){
 
 
 
+function checkbeforedelete(){
+    let isExecuted = confirm("Are you sure to execute this action?");
+    
+    if (isExecuted) {
+        deletebuttonll=document.getElementsByName('deletebutton');                     
+           foundit= $(deletebuttonll).closest('.productcard')[0];
+           $(foundit).remove();
+          } else {
+            alert("Action canceled");
+          }
+        }
